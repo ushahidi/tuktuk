03373182e7433fab81b8ac4e9eecd63ac722e0d7
Moses Ngone <moses@mospired.com>
2017-01-04 15:29:00 -0500

    remove ionic storage and prep for ionic

--- src/providers/report-provider.ts
+++ src/providers/report-provider.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { SQLite } from 'ionic-native';
 import 'rxjs/add/operator/map';
 import uuid from 'uuid/v4';
 
@@ -12,59 +11,60 @@ for more info on providers and Angular 2 DI.
 @Injectable()
 export class ReportProvider {
 
-  constructor(public storage: SQLite) {
+  constructor() {
 
-    this.storage = new SQLite();
-    this.storage
-    .openDatabase({ name: "data.db", location: "default" })
-    .then(() => {
-      this.storage
-      .executeSql("CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, address TEXT)", [])
-      .catch((error) => {
-        console.error('Unable to execute sql: ', error);
-      })
-    })
-    .then(() => {
-      console.info('DB OPENED SUCCESSFULLY')
-    })
-    .catch((error) => {
-      console.error('Unable to open database: ', error);
-    })
+    // const db = new SQLite();
+    // db.openDatabase({ name: "data.db", location: "default" })
+    // .then(() => {
+    //   db.executeSql("CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, address TEXT)", [])
+    //   .catch((error) => {
+    //     console.error('Unable to execute sql: ', error);
+    //   })
+    // })
+    // .then(() => {
+    //   console.info('DB OPENED SUCCESSFULLY')
+    // })
+    // .catch((error) => {
+    //   console.error('Unable to open database: ', error);
+    // })
+    // this.storage = db;
 
   }
 
-  public getAll() {
-    return new Promise((resolve, reject) => {
-      this.storage.executeSql("SELECT * FROM reports", []).then((data) => {
-        let reports = [];
-        if (data.rows.length > 0) {
-          for (let i = 0; i < data.rows.length; i++) {
-            reports.push({
-              id: data.rows.item(i).id,
-              firstname: data.rows.item(i).description,
-              lastname: data.rows.item(i).address
-            });
-          }
-        }
-        resolve(reports);
-      }, (error) => {
-        reject(error);
-      });
-    });
+  public fetch() {
+    // return new Promise((resolve, reject) => {
+    //   this.storage.executeSql("SELECT * FROM reports", []).then((data) => {
+    //     let reports = [];
+    //     if (data.rows.length > 0) {
+    //       for (let i = 0; i < data.rows.length; i++) {
+    //         reports.push({
+    //           id: data.rows.item(i).id,
+    //           firstname: data.rows.item(i).description,
+    //           lastname: data.rows.item(i).address
+    //         });
+    //       }
+    //     }
+    //     console.log(`REPORTS ${reports}`)
+    //     resolve(reports);
+    //   }, (error) => {
+    //     reject(error);
+    //   });
+    // });
   }
 
-  public set(description: string, address: string) {
+  public add(description: string, address: string) {
     const id = uuid();
     console.info(`SAVING ID: ${id}`)
-    return new Promise((resolve, reject) => {
-      this.storage
-      .executeSql("INSERT INTO reports (description, address) VALUES (?, ?)", [ description, address])
-      .then((data) => {
-        resolve(data);
-      }, (error) => {
-        reject(error);
-      });
-    });
+    // return new Promise((resolve, reject) => {
+    //   this.storage
+    //   .executeSql("INSERT INTO reports (description, address) VALUES (?, ?)", [ description, address])
+    //   .then((data) => {
+    //     console.log(`INSERTED DATA: ${JSON.stringify(data)}`)
+    //     resolve(data);
+    //   }, (error) => {
+    //     reject(error);
+    //   });
+    // });
   }
 
 }
